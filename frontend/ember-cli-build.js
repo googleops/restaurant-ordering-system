b971e2d48be6ae7abb66ed53a9d8f32d6944b1e1
Andrew <andrewfebrian3@gmail.com>
2024-04-27 03:34:38 +0000

    feat: tailwind css setup

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -9,16 +9,31 @@ module.exports = function (defaults) {
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
-    staticEmberSource: true,
     skipBabel: [
       {
         package: 'qunit',
       },
     ],
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      config: 'postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
   });
 };
